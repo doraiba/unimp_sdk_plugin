b68a9ec1d0f4a2c61a39ee3764ff94b454b3076b
djt <1448897644@qq.com>
2023-12-31 11:51:29 +0800

    test

---
+++ example/android/app/src/main/kotlin/com/github/doraiba/unimp_sdk_plugin_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.github.doraiba.unimp_sdk_plugin_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ example/android/app/src/main/kotlin/com/github/unimp_sdk_plugin_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.github.unimp_sdk_plugin_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
